79b7ac2d5030e50ea8e2200b6a9bd8bfeffde66b
cgupte1 <cgupte1@bloomberg.net>
2022-02-09 11:08:11 -0800

    Expose prometheus metrics

--- gateway-ha/src/main/java/com/lyft/data/gateway/ha/HaGatewayLauncher.java
+++ gateway-ha/src/main/java/com/lyft/data/gateway/ha/HaGatewayLauncher.java
@@ -1,12 +1,21 @@
 package com.lyft.data.gateway.ha;
 
+import com.google.inject.Injector;
 import com.lyft.data.baseapp.BaseApp;
 import com.lyft.data.gateway.ha.config.HaGatewayConfiguration;
 import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
 import io.dropwizard.views.ViewBundle;
+import io.prometheus.client.CollectorRegistry;
+import io.prometheus.client.dropwizard.DropwizardExports;
+import io.prometheus.client.exporter.MetricsServlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HaGatewayLauncher extends BaseApp<HaGatewayConfiguration> {
+  private static final Logger logger = LoggerFactory.getLogger(HaGatewayLauncher.class);
 
   public HaGatewayLauncher(String... basePackages) {
     super(basePackages);
@@ -17,6 +26,19 @@ public class HaGatewayLauncher extends BaseApp<HaGatewayConfiguration> {
     super.initialize(bootstrap);
     bootstrap.addBundle(new ViewBundle<>());
     bootstrap.addBundle(new AssetsBundle("/assets", "/assets", null, "assets"));
+  }
+
+  @Override
+  protected void applicationAtRun(HaGatewayConfiguration configuration,
+                                  Environment environment,
+                                  Injector injector) {
+    // Register prometheus metrics
+    logger.info("Registering prometheus metrics endpoint at /prometheusMetrics");
+    CollectorRegistry collectorRegistry = new CollectorRegistry();
+    collectorRegistry.register(new DropwizardExports(environment.metrics()));
+    environment.admin()
+            .addServlet("prometheusMetrics", new MetricsServlet(collectorRegistry))
+            .addMapping("/prometheusMetrics");
   }
 
   public static void main(String[] args) throws Exception {
